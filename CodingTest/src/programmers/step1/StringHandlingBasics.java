884b283731edb23efb79afb32669e83096891123
SeHoonBae <47914153+SeHoonBae@users.noreply.github.com>
2020-12-30 08:58:21 +0900

    문자열 다루기 기본

---
+++ CodingTest/src/programmers/step1/StringHandlingBasics.java
@@ -0,0 +1,28 @@
+package programmers.step1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringHandlingBasics {
+
+    @Test
+    public void main(){
+        Assertions.assertEquals(solution("a234"), false);
+        Assertions.assertEquals(solution("1234"), true);
+    }
+
+    // 1. s의 길이가 4 또는 6인지 비교
+    // 2. s가 숫자로 변환 가능한지 비교
+    public boolean solution(String s) {
+        if(s.length() == 4 || s.length() == 6){
+            try {
+                Integer.parseInt(s);
+                return true;
+            }catch (Exception e){
+                return false;
+            }
+        }
+        return false;
+    }
+
+}
