39ea3c7bdd7ca0cb045610609dc002aa2369fc0b
SeHoonBae <47914153+SeHoonBae@users.noreply.github.com>
2020-12-30 21:22:42 +0900

    수박수박수박수박수박수?

---
+++ CodingTest/src/programmers/step1/ClapBackwards.java
@@ -0,0 +1,31 @@
+package programmers.step1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ClapBackwards {
+
+    @Test
+    public void main(){
+
+        Assertions.assertEquals(solution(3),"수박수");
+        Assertions.assertEquals(solution(4),"수박수박");
+
+    }
+
+    public String solution(int n) {
+        String answer = "";
+
+        for(int i=1; i<=n; i++)
+            answer += i%2==0?"박":"수";
+
+        return answer;
+        
+        // 아래는 다른사람 풀이
+        // char형으로 n개의 절반만큼 배열 생성 n/2에 +1을 해준건 1부터 시작하기 위함
+        // char 크기만큼 수박을 생성 한 후 substring으로 길이 자름
+//        return new String(new char [n/2+1]).replace("\0", "수박").substring(0,n);
+
+    }
+
+}
