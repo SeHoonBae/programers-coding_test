b47dd4559d32cfff15c7f1cf54fd36cc1edbf459
SeHoonBae <47914153+SeHoonBae@users.noreply.github.com>
2021-03-15 14:37:03 +0900

    실패율

---
+++ CodingTest/src/programmers/step1/FailureRate.java
@@ -0,0 +1,71 @@
+package programmers.step1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+public class FailureRate {
+
+    @Test
+    public void main(){
+        Assertions.assertArrayEquals(solution(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3}), new int[]{3,4,2,1,5});
+        Assertions.assertArrayEquals(solution(4, new int[]{4,4,4,4,4}), new int[]{4,1,2,3});
+    }
+
+    public int[] solution(int N, int[] stages) {
+        int total = stages.length;
+        int index = 0;
+        int[] answer = new int[N];
+        double[] temp = new double[N+2];
+        ArrayList<Stage> fail = new ArrayList<Stage>();
+
+        // 1. 각 스테이지별 남은 사람 수를 구한다.
+        for(int stage : stages){
+            temp[stage]++;
+        }
+
+        // 2. 각 스테이지별 실패율을 구한다.
+        for(int i=1; i<temp.length-1; i++){
+            Stage s = new Stage(i, temp[i] / total);
+            fail.add(s);
+            total -= temp[i];
+        }
+
+        // 3. 각 스테이지별 실패율을 내림차순 정렬한다.
+        Collections.sort(fail);
+
+        for(Stage s : fail){
+            answer[index++] = s.getNum();
+        }
+
+        return answer;
+    }
+
+    public class Stage implements Comparable<Stage>{
+        private int num;
+        private double failureRate;
+
+        public Stage(int num, double failureRate){
+            this.num = num;
+            this.failureRate = failureRate;
+        }
+
+        public double getFailureRate(){
+            return failureRate;
+        }
+
+        public int getNum(){
+            return num;
+        }
+
+        @Override
+        public int compareTo(Stage o) {
+            if(o.getFailureRate() - getFailureRate() > 0) return 1;
+            else if(o.getFailureRate() - getFailureRate() < 0) return -1;
+            else return 0;
+        }
+
+    }
+
+}
