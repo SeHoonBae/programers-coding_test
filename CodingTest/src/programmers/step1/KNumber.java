240f22af3122757c0af801e27af051a4dd129c5a
SeHoonBae <47914153+SeHoonBae@users.noreply.github.com>
2021-09-26 19:01:19 +0900

    K번째 수 개선

--- CodingTest/src/programmers/step1/KNumber.java
+++ CodingTest/src/programmers/step1/KNumber.java
@@ -18,27 +18,41 @@ public class KNumber {
     // 2. 1의 배열을 정렬한다.
     // 3. 2의 배열에서 k번째 수를 answer배열에 삽입
     // 4. 1~3의 방식을 commands의 행의 갯수만큼 반복
+//    public int[] solution(int[] array, int[][] commands) {
+//        // return 할 answer 배열은 매개변수의 commands의 행의 갯수와 같다.
+//        int[] answer = new int[commands.length];
+//
+//        // 4.
+//        for(int i=0; i<commands.length; i++){
+//            int first = commands[i][0]; // 자를 시작 요소
+//            int last = commands[i][1]; // 자를 마지막 요소
+//            int index = first-1; // temp에 값을 넣을 때 사용 할 인덱스
+//            int[] temp = new int[last - first + 1];
+//
+//            // 1.
+//            for(int j=0; j<temp.length; j++)
+//                temp[j] = array[index++];
+//            // int first ~ 바로 위 까지 내용을
+//            // int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]); 로 줄일 수 있음
+//            // 2.
+//            Arrays.sort(temp);
+//
+//            // 3.
+//            answer[i] = temp[commands[i][2]-1];
+//
+//        }
+//
+//        return answer;
+
+
     public int[] solution(int[] array, int[][] commands) {
-        // return 할 answer 배열은 매개변수의 commands의 행의 갯수와 같다.
         int[] answer = new int[commands.length];
 
-        // 4.
         for(int i=0; i<commands.length; i++){
-            int first = commands[i][0]; // 자를 시작 요소
-            int last = commands[i][1]; // 자를 마지막 요소
-            int index = first-1; // temp에 값을 넣을 때 사용 할 인덱스
-            int[] temp = new int[last - first + 1];
 
-            // 1.
-            for(int j=0; j<temp.length; j++)
-                temp[j] = array[index++];
-            // int first ~ 바로 위 까지 내용을
-            // int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]); 로 줄일 수 있음
-            // 2.
-            Arrays.sort(temp);
-
-            // 3.
-            answer[i] = temp[commands[i][2]-1];
+            int[] tmp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(tmp);
+            answer[i] = tmp[commands[i][2]-1];
 
         }
 
