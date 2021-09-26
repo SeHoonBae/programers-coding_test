0bd4859086c790cae9d9182a76d5009e8fbb1425
SeHoonBae <47914153+SeHoonBae@users.noreply.github.com>
2021-09-26 18:50:50 +0900

    소수만들기 개선

--- CodingTest/src/programmers/step1/DecimalMake.java
+++ CodingTest/src/programmers/step1/DecimalMake.java
@@ -34,5 +34,55 @@ public class DecimalMake {
         return answer;
     }
 
+//    public int solution(int[] nums) {
+//        int answer = 0;
+//
+//        boolean[] visited = new boolean[nums.length];
+//
+//        answer = decimalCnt(nums, visited, 0, 3);
+//
+//        return answer;
+//    }
+//
+//    public int decimalCnt(int[] arr, boolean[] visited, int start, int cnt){
+//
+//        int result = 0;
+//
+//        if( cnt == 0){
+//
+//            int tmp = 0;
+//            for(int i=0; i<arr.length; i++){
+//                if(visited[i])
+//                    tmp += arr[i];
+//            }
+//
+//            if(isDecimal(tmp))
+//                result++;
+//        }else{
+//            for (int i=start; i<arr.length; i++){
+//                visited[i] = true;
+//                result += decimalCnt(arr, visited, i+1, cnt-1);
+//                visited[i] = false;
+//            }
+//        }
+//
+//        return result;
+//
+//    }
+//
+//    public boolean isDecimal(int num){
+//        boolean result = true;
+//
+//        for(int i = 2; i*i<=num; i++){
+//
+//            if(num%i==0){
+//                result = false;
+//                break;
+//            }
+//
+//        }
+//        return result;
+//    }
+
 }
 //출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
